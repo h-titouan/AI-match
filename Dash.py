5881b31f7133483f233432cb89024f0ecd8270a9
h-titouan <houdetitouan@yahoo.fr>
2022-10-18 12:59:19 +0200

    Multipage Dash

--- Dash.py
+++ Dash.py
@@ -1,5 +1,6 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
+# Imports
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
@@ -10,12 +11,12 @@ import plotly.graph_objects as go
 
 app = Dash(__name__)
 
-#rearrangement du dataframe pour le plot
+# Few usefull
 data = model_ready
 dataG = dataG
 colonnes = list(dataG.columns)
 
-# Emplacement des images
+# Image directory
 EasyDate = 'C:/Users/houde/PycharmProjects/pythonProject2/Easy Date/EasyDate.png'
 AI_match = 'C:/Users/houde/PycharmProjects/pythonProject2/Easy Date/AI_match.png'
 
@@ -24,10 +25,16 @@ AI_match = 'C:/Users/houde/PycharmProjects/pythonProject2/Easy Date/AI_match.png
 # App layout
 app.layout = html.Div([
 
+    # Show images
     html.Img(src=dashF.b64_image(EasyDate),style={'width':"20%"}),
     html.Img(src=dashF.b64_image(AI_match),style={'width':"10%"}),
 
-html.Div([
+    # Create Multipage on App
+    dcc.Tabs([
+        dcc.Tab(label='Page 1', children=[
+
+    # Columns Selector
+    html.Div([
 
         html.Label(['Choisissez une variable :'],
         style={'font-weight': 'bold', "text-align": "center"}),
@@ -60,25 +67,38 @@ html.Div([
         # Graphique du radar
         dcc.Graph(id='radar', className="radar",  style={'float': 'left','margin': 'auto'})], style ={"width" : "60%"})
 
+        # Close Page 1
+        ]),
+        dcc.Tab(label='Page 2', children=[
+
+        # Close Page 2
+        ])
+    # Close Tabs
+    ])
 ])
 
 #---------------------------------------------------------------
 # Connecting the Dropdown values to the graph
+
+# Barplot Callback
 @app.callback(
     Output(component_id='barplot', component_property='figure'),
     Input(component_id='select_cols', component_property='value')
     )
 
+# Barplot reactive function
 def build_bar(column_chosen):
     df = dashF.interactive_bar(column_chosen)
     fig = px.bar(df, x=column_chosen, y="Taux_match", color_discrete_sequence= ["#E0115F"])
     return fig
 
+# Radar Callback
 @app.callback(
     Output(component_id='radar', component_property='figure'),
     Input(component_id='checklist', component_property='value')
      )
 
+# Radar reactive function
 def build_radar(cols):
     radar = dashF.radar_fig(cols)
     return radar
